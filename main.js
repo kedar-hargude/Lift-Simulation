56611d57b562beffe8e014e45c9ee933dfebbdb7
Kedar Hargude <hargude481@gmail.com>
2023-02-21 07:37:51 +0530

    create input page with submiting values

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
-"use strict";
+// "use strict";
 
-const callBtn4 = document.querySelector(".btn4");
-const callBtn3 = document.querySelector(".btn3");
-const callBtn2 = document.querySelector(".btn2");
-const callBtn1 = document.querySelector(".btn1");
+// const callBtn4 = document.querySelector(".btn4");
+// const callBtn3 = document.querySelector(".btn3");
+// const callBtn2 = document.querySelector(".btn2");
+// const callBtn1 = document.querySelector(".btn1");
 
-const door = document.querySelector(".door");
+// const door = document.querySelector(".door");
 
-function goto4() {
-    door.style.bottom = '305px';
-}
+// function goto(floorNo) {
+//     // door.style.bottom = '305px';
+//     door.style.bottom = `${floorNo * 150 + 5}px`;
+//     // door.style.transition = `bottom ${(floorNo + 1) * 2}s`
+// }
 
-callBtn4.addEventListener("click", goto4)
+
+// callBtn4.addEventListener("click", () => goto(3))
+// callBtn3.addEventListener("click", () => goto(2))
+// callBtn2.addEventListener("click", () => goto(1))
+// callBtn1.addEventListener("click", () => goto(0))
+
+const inputPage = document.querySelector(".input-page");
+const liftsInput = document.getElementById("lift-input");
+const floorInput = document.getElementById("floor-input");
+const inputsSubmit = document.querySelector(".inputsSubmit");
+
+// input page submitting inputs
+inputsSubmit.addEventListener("click", () => {
+    if (liftsInput.value && floorInput.value) {
+        // console.log(floorInput.value);
+        console.log(`lifts: ${liftsInput.value}, floors: ${floorInput.value}`);
+        inputPage.style.display = 'none';
+    }
+})
