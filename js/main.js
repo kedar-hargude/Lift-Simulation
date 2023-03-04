0a0ba902e356829a970e14912f37e8f5a6033f11
Kedar Hargude <hargude481@gmail.com>
2023-03-04 13:39:17 +0530

    build navbar and render changed input values

--- js/main.js
+++ js/main.js
@@ -24,26 +24,36 @@
 
 // let numberOfLifts = liftsInput.value;
 // let numberOfFloors = floorInput.value;
+// TODO: remove these predefined values
 let numberOfLifts = 5;
 let numberOfFloors = 8;
 
-console.log("before: page rendered")
-console.log(`lift value: ${numberOfLifts}`)
-console.log(`floor value: ${numberOfFloors}`)
+// console.log("before: page rendered")
+// console.log(`lift value: ${numberOfLifts}`)
+// console.log(`floor value: ${numberOfFloors}`)
 
+// navbar buttons
 const liftInputUpdate = document.querySelector("#lift-input-update");
 const floorInputUpdate = document.querySelector("#floor-input-update");
-const updateBtn = document.querySelector(".update-btn")
+const updateBtn = document.querySelector(".update-btn");
 
 liftInputUpdate.value = numberOfLifts;
 floorInputUpdate.value = numberOfFloors;
 
+const liftOutputTag = document.querySelector('#lift-output');
+
+function buildNewLiftContainer(numberOfLifts, numberOfFloors) {
+    liftOutputTag.innerHTML = "Lifts: " + numberOfLifts + ' Floors: ' + numberOfFloors;
+}
+
+// handle update button click
 updateBtn.addEventListener("click", () => {
-    // TODO: put those checks here of min and max values
     numberOfLifts = liftInputUpdate.value;
     numberOfFloors = floorInputUpdate.value;
+    
+    // do nothing if passed values are beyong bounds
+    if (numberOfLifts < 1 || numberOfLifts > 5 || numberOfFloors < 2 || numberOfFloors > 10) return;
 
-    console.log("After click")
-    console.log(`lifts updated value: ${numberOfLifts}`)
-    console.log(`floors updated value: ${numberOfFloors}`)
+    buildNewLiftContainer(numberOfLifts, numberOfFloors);
+
 })
