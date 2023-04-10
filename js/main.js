4be693406e37e25890e8c0c895d7d6bc642bfbbb
Kedar Hargude <hargude481@gmail.com>
2023-04-10 06:21:22 +0530

    use single file for js

--- js/main.js
+++ js/main.js
@@ -19,49 +19,81 @@
 // callBtn2.addEventListener("click", () => goto(1))
 // callBtn1.addEventListener("click", () => goto(0))
 
-// console.log(liftsInput.value)
-// console.log(floorInput.value)
+// const liftsInput = document.getElementById("lift-input");
+// const floorInput = document.getElementById("floor-input");
 
-// let numberOfLifts = liftsInput.value;
-// let numberOfFloors = floorInput.value;
-// TODO: remove these predefined values
-let numberOfLifts = 5;
-let numberOfFloors = 8;
-
-// console.log("before: page rendered")
-// console.log(`lift value: ${numberOfLifts}`)
-// console.log(`floor value: ${numberOfFloors}`)
+// input page buttons
+const inputPage = document.querySelector(".input-page");
+const liftsInput = document.getElementById("lift-input");
+const floorInput = document.getElementById("floor-input");
+const inputsSubmit = document.querySelector(".inputsSubmit");
 
 // navbar buttons
 const liftInputUpdate = document.querySelector("#lift-input-update");
 const floorInputUpdate = document.querySelector("#floor-input-update");
 const updateBtn = document.querySelector(".update-btn");
 
-liftInputUpdate.value = numberOfLifts;
-floorInputUpdate.value = numberOfFloors;
+// input page submitting inputs
+inputsSubmit.addEventListener("click", () => {
 
-const liftOutputTag = document.querySelector('#lift-output');
+    if ((liftsInput.value < 1 || liftsInput.value > 5) || (floorInput.value < 2 || floorInput.value > 10)) {
+        location.reload();
+    } else {
+        console.log(`lifts: ${liftsInput.value}, floors: ${floorInput.value}`);
+        inputPage.style.display = 'none';
+        liftInputUpdate.value = liftsInput.value;
+        floorInputUpdate.value = floorInput.value;
+        buildNewLiftContainer(liftsInput.value, floorInput.value);
+    }
+});
 
-function buttonClickHandler( isUpPressed, floorNumber) {
+console.log(liftsInput.value)
+console.log(floorInput.value)
+
+// let numberOfLifts = liftsInput.value;
+// let numberOfFloors = floorInput.value;
+// TODO: remove these predefined values
+// let numberOfLifts = 5;
+// let numberOfFloors = 8;
+
+// console.log("before: page rendered")
+// console.log(`lift value: ${numberOfLifts}`)
+// console.log(`floor value: ${numberOfFloors}`)
+
+// // navbar buttons
+// const liftInputUpdate = document.querySelector("#lift-input-update");
+// const floorInputUpdate = document.querySelector("#floor-input-update");
+// const updateBtn = document.querySelector(".update-btn");
+
+// liftInputUpdate.value = numberOfLifts;
+// floorInputUpdate.value = numberOfFloors;
+
+
+function liftCallClickHandler( isUpPressed, floorNumber) {
     console.log(`Floor: ${floorNumber}, ${isUpPressed? "Up" : "Down"} key pressed.`)
 }
 
 function buildNewLiftContainer(numberOfLifts, numberOfFloors) {
-    // liftOutputTag.innerHTML = "Lifts: " + numberOfLifts + ' Floors: ' + numberOfFloors;
     const buildingContainer = document.querySelector("#building-container");
     buildingContainer.innerHTML = "";
     
-    for (let count = 1; count <= numberOfFloors; count++) {
+    for (let count = numberOfFloors; count > 0; count--) {
         buildingContainer.innerHTML += `
         <div class="floor">
             <div class="floor-info-box">
                 <p class="floor-words">Floor</p>
                 <p class="floor-number">${count}</p>
-                <button onclick="buttonClickHandler(true, ${count})" class="btn up-btn">Up</button>
-                <button onclick="buttonClickHandler(false, ${count})" class="btn down-btn">Down</button>
+                <button onclick="liftCallClickHandler(true, ${count})" class="btn up-btn">Up</button>
+                <button onclick="liftCallClickHandler(false, ${count})" class="btn down-btn">Down</button>
             </div>
         </div>`
     }
+
+    // generate multiple lifts here
+    buildingContainer.innerHTML += `
+    <div class="lifts-container-block">
+        <div class="lift"></div>
+    </div>`
 }
 
 // handle update button click
